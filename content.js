80de3fcd52be1962c2e3ea3056bc017722871766
Daryll Tumambing <daryllft19@gmail.com>
2016-12-07 21:51:23 +0800

    added notif popups, movie list will not reset even after extension closes, hides movie to guess

--- content.js
+++ content.js
@@ -1,41 +1,65 @@
 var titles = [];
 var chosen = [];
+var movie = '';
 
 init = function(){
 	titles = [];
+	movie = '';
+	chosen = [];
+	
 	a = document.getElementsByClassName("klitem");
 	for(i = 0; i < a.length;i++){titles.push(a[i].title + "\n")};
-
+	
 	chrome.runtime.sendMessage({titles:titles}, function(response) {
 		console.info("Initializing!");
 	});
 }
 
 getNext = function(list){
-	next = -1 
+	next = ''
 	if(list.length == 0){
-		alert("NO MOVIES IN LIST");
+		chrome.runtime.sendMessage({movie:''}, function(response) {
+			console.info("No movies!");
+		});
 		return;
 	}
 
 	rand_index = Math.round(Math.random()*list.length);
 	chosen.push(list[rand_index])
 	next = list[rand_index];
+	movie = next;
 	list.splice(rand_index,1);
 
-	chrome.runtime.sendMessage({msg:next}, function(response) {
+	chrome.runtime.sendMessage({movie:next}, function(response) {
 		console.info("Next!");
+	});
+}
+
+getItem = function(){
+	dMsg = {items:titles.length}
+	if(movie)
+	{
+		dMsg.movie = movie;
+	}
+	chrome.runtime.sendMessage(dMsg, function(response) {
+		console.info("Getting items...");
 	});
 }
 
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
 		console.info("Content.js: " + request);
-		if(request == 'init'){
-			init()
-		}
-		else if(request == 'next'){
-			getNext(titles)
+		switch(request)
+		{
+			case 'init':
+				init();
+				break;
+			case 'next':
+				getNext(titles);
+				break;
+			case 'items':
+				getItem();
+				break;
 		}
     }
 );
--- popup.js
+++ popup.js
@@ -1,19 +1,62 @@
-var chosen = [];
 var titles = [];
+var movie = '';
 
 window.onload = function() {
-    document.getElementById('init').onclick = function() {	
+	$('#init-success').hide();
+	$('#init-danger').hide();
+	chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, "items");
+    });
+	
+    $('#init').click(function() {	
 		chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, "init");
+			chrome.tabs.sendMessage(tabs[0].id, "items");
         });
-	
 		return;
-     }; 
+     }); 
 
-    document.getElementById('next').onclick = function() {
-		getNext(titles);
-     }; 
+    $('#next').click(function() {
+		chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, "next");
+			chrome.tabs.sendMessage(tabs[0].id, "items");
+        });
+		return;
+     }); 
 
+    $('#guess').hover(function() {
+		$('#guess').css('color','black').css('text-shadow','none');
+    }).mouseout(function(){
+		$('#guess').css('color','transparent').css('text-shadow','0 0 10px #000');
+	}); 	
+
+	$('#guess').click(function() {
+		window.open('http://www.google.com/search?q='+movie,'_blank');
+	});
+	 
+	disableNext();
+}
+
+disableNext = function(){
+	if($('#items').prop('innerHTML') == 0)
+	{
+		$('#next').prop('disabled',true);
+	}
+	else
+	{
+		$('#next').prop('disabled', false);
+	}
+}
+
+getMovie = function(movie){
+	if (movie == '')
+	{
+		$('#guess').prop('innerHTML','N/A');
+	}
+	else
+	{
+		$('#guess').prop('innerHTML',movie);
+	}
 
 }
 
@@ -21,30 +64,43 @@ chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if ('titles' in request)
 		{
-			titles = request.titles
+			titles = request.titles;
+			movie = '';
 			if(titles.length > 0)
 			{
-				alert('Initialized ' + titles.length + ' movies!');
+				$('#items-success').prop('innerHTML',titles.length);
+				$('#init').prop('disabled',true);
+				$('#init-success').slideDown().delay(3000).slideUp(function(){
+					$('#init').prop('disabled',false);
+				});
 			}
 			else
 			{
-				alert('Nothing initialized!');
+				$('#init').prop('disabled',true);
+				$('#init-danger').slideDown().delay(2000).slideUp(function(){
+					$('#init').prop('disabled',false);
+				});
+			}
+			getMovie('');
+		}
+		if ('movie' in request)
+		{
+			movie = request.movie;
+			if(movie.length > 0)
+			{
+				getMovie(movie);
+				//alert(request.movie);
+			}
+			else
+			{
+				alert('No more movies!');
+				getMovie('');
 			}
 		}
+		if ('items' in request)
+		{
+			getMovie(movie);
+			$('#items').prop('innerHTML',request.items);
+			disableNext();
+		}
 });
-
-
-getNext = function(list){
-next = -1 
-if(list.length == 0){
-	alert("NO MOVIES IN LIST");
-	return;
-	}
-
-rand_index = Math.round(Math.random()*list.length);
-chosen.push(list[rand_index])
-next = list[rand_index];
-list.splice(rand_index,1);
-
-alert(next);
-}
